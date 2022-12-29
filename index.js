2f546f4b915e974caae2a641fb0043bb24798bc8
venkatesh-tech <kolapallinagavenkatesh@gmail.com>
2022-12-29 08:10:15 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const redux = require("redux");
+// const createStore = redux.createStore
+// store
+const createStore = redux.legacy_createStore;
+
+//Action
+const CAKE_ORDERED = "CAKE_ORDERED"; // constant
+
+function orderCake() {
+  // action taker returns object
+  return {
+    type: CAKE_ORDERED, // type property action
+    quantity: 1,
+  };
+}
+
+//Reducer
+const initialState = {
+  numberOfCakes: 10,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        state: state.numberOfCakes - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer); //resposnsibility 1
+console.log("InitialState", store.getState()); //resposnsibility 2
+
+const unSubscribe = store.subscribe(() =>
+  console.log("updated State ", store.getState())
+); //resposnsibility 4
+
+store.dispatch(orderCake()); //resposnsibility 3
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+
+unSubscribe(); //resposnsibility 5

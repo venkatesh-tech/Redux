d662ce871b7bd7bd1b04b70972ea4e750d1e98ce
venkatesh-tech <kolapallinagavenkatesh@gmail.com>
2022-12-29 10:08:02 +0530

    using single reducer

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const bindActionCreators = redux.bindActionCreators;
 //Action
 const CAKE_ORDERED = "CAKE_ORDERED"; // constant
 const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED"; // constant
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
 
 function orderCake() {
   // action taker returns object
@@ -23,9 +25,24 @@ function restockCake(qty = 1) {
   };
 }
 
+function orderIcecream(qty = 1) {
+  return {
+    type: ICECREAM_ORDERED,
+    payload: qty,
+  };
+}
+
+function restockIcecream(qty = 1) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qty,
+  };
+}
+
 //Reducer
 const initialState = {
   numberOfCakes: 10,
+  numberOfIcecreams: 20,
 };
 
 const reducer = (state = initialState, action) => {
@@ -39,6 +56,16 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         numberOfCakes: state.numberOfCakes + action.payload,
+      };
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numberOfIcecreams: state.numberOfIcecreams - action.payload,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numberOfIcecreams: state.numberOfIcecreams + action.payload,
       };
     default:
       return state;
@@ -58,10 +85,16 @@ const unsubscribe = store.subscribe(() =>
 
 // store.dispatch(restockCake(3));
 
-const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch);
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderIcecream, restockIcecream },
+  store.dispatch
+);
 actions.orderCake();
 actions.orderCake();
 actions.orderCake();
 actions.restockCake(3);
+
+actions.orderIcecream(2);
+actions.restockIcecream(2);
 
 unsubscribe(); //resposnsibility 5
